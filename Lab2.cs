3c5f64efa8b7f541a3b99a9a7f9e533bb7543354
K0teiize <ktv110687@gmail.com>
2024-10-12 16:04:43 +0600

    Lab2.cs

--- Lab2.cs
+++ Lab2.cs
@@ -1,19 +1,26 @@
-﻿using System;
+using System;
 public class Prog
 {
     public static void Main(string[] args)
     {
+        
         int num = int.Parse(Console.ReadLine());
 
-        int nechet=0;
+        int nechet = 0, efive=0;
         for (int i = 0; i < num; i++)
         {
             int a = int.Parse(Console.ReadLine());
+            //Проверка на нечетность
             if (a % 2 != 0)
             {
                 nechet++;
             }
-            
+            //Оканчивается ли элемент на 5
+            if (a % 10 == 5)
+            {
+                efive++;
+            }
+
         }
         if (nechet == num)
         {
@@ -23,11 +30,7 @@ public class Prog
         {
             Console.WriteLine("Не все элементы нечетные");
         }
-
-
-
-
-
+        Console.WriteLine("Кол-во элементов оканчивающихся на 5:" +" "+ efive);
 
 
 
