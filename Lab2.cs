7ce5cedfbc32b09f53a1ffec2f7af1a77fbff4a0
K0teiize <ktv110687@gmail.com>
2024-10-12 15:42:57 +0600

    Add files via upload

---
+++ Lab2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public class Prog
+{
+    public static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        int nechet=0;
+        for (int i = 0; i < num; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 != 0)
+            {
+                nechet++;
+            }
+            
+        }
+        if (nechet == num)
+        {
+            Console.WriteLine("Все элементы нечетные");
+        }
+        else
+        {
+            Console.WriteLine("Не все элементы нечетные");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
